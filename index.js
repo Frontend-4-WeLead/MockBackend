9944828caeb12db7c990107d6c325b81cc798be9
NikosGour <nikosgournak@gmail.com>
2023-11-23 00:01:32 +0200

    Latest

--- index.js
+++ index.js
@@ -5,25 +5,54 @@ let port = 3000
 
 app.use(cors())
 
-let x = require('./test.json')
+let shops = require('./shop.json')
 
-app.get('/x', (req, res) => {
-	console.log("req x")
-	res.send({ x: x.x })
+app.get('/all', (req, res) => {
+
+	console.log("Requested all shops")
+	res.send({ shops: shops })
 }
 )
-app.get('/y', (req, res) => {
-	console.log("req y")
-	res.send({ y: x.y })
+app.get('/:id', (req, res) => {
+
+	let id = req.params.id
+	if (isNaN(id)) {
+		res.status(400).send({ error: "Id must be a number" })
+		return
+	}
+	if (id < 0 || id >= shops.length) {
+		res.status(404).send({ error: "No shop with id " + id + ", only " + (shops.length - 1) + " shops" })
+		return
+	}
+
+	id = parseInt(id)
+
+	console.log("Requested shop with id " + id)
+	let shop = shops[id];
+	res.send(shop)
 }
 )
-app.get('/z', (req, res) => {
-	console.log("req z")
-	res.send({ z: x.z })
+app.get('/:id/products', (req, res) => {
+
+	let id = req.params.id
+	if (isNaN(id)) {
+		res.status(400).send({ error: "Id must be a number" })
+		return
+	}
+	if (id < 0 || id >= shops.length) {
+		res.status(404).send({ error: "No shop with id " + id })
+		return
+	}
+
+	id = parseInt(id)
+	let products = shops[id].products
+	console.log("Requested products of shop with id " + id)
+	res.send(products)
 }
 )
 
 app.listen(port, () => {
+	console.log(shops);
 	console.log(`Example app listening at http://localhost:${port}`)
 }
 )
